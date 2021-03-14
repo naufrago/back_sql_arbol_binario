3f6eb4fa434f9f78a5b344fba0a6cc56d6e1a79a
Yeison Aguirre Osorio <naufragodev@gmail.com>
2021-03-13 19:38:40 -0500

    base de proyecto

---
+++ src/main/java/com/back_sql_arbol_binario/BackSqlArbolBinarioApplication.java
@@ -0,0 +1,13 @@
+package com.back_sql_arbol_binario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSqlArbolBinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSqlArbolBinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back_sql_arbol_binario/BackSqlArbolBinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_sql_arbol_binario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSqlArbolBinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
